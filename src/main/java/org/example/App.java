2893be998ddc511d0c1ec903329247ae2c9d8f94
Korepanov Еvgeniy <es.korepanov@gmail.com>
2020-10-15 00:27:49 +0400

    fix package App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,17 +1,10 @@
 package org.example;
 
-import javafx.application.Application;
-import org.example.context.Context;
 import org.example.view.Bot;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         ApiContextInitializer.init();
